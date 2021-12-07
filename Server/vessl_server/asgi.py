cfd2b42347d658937575814f3077ac83b95d6013
Johan Monster <linkthrough@hotmail.com>
2021-12-07 16:47:13 +0100

    Added some comments

--- Server/vessl_server/asgi.py
+++ Server/vessl_server/asgi.py
@@ -9,22 +9,31 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os, sys
 
+# JOHAN Question: Why is SessionMiddlewareStack used, and not AuthMiddlewareStack, as 
+#   in https://channels.readthedocs.io/en/stable/topics/routing.html ?
 from channels.sessions import SessionMiddlewareStack
+# Import ProtocolTypeRouter (the recommended default root router application)
+# 	and URLRouter
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.core.asgi import get_asgi_application
 from django.conf.urls import url
+
 
 # The imports need this as it imports from a sibling directory
 #from vesslserver.vessl_api import VESSL_API_Consumer
 sys.path.append(os.path.abspath("../vessl_api"))
 from vessl_api.vessl_api import VESSL_API_Consumer
 
-
+# Setting the OS environment variable `DJANGO_SETTINGS_MODULE` to `vessl_server.settings`, 
+#   so that Django knows where to look for the server settings.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vessl_server.settings')
 
 application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
     "http": get_asgi_application(),
+
+    # Specifying that the VESSL_API_Consumer app uses websockets
     "websocket": SessionMiddlewareStack(
         URLRouter([
             url("api", VESSL_API_Consumer.as_asgi())
